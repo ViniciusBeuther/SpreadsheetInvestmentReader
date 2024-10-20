7a95f67320f77824d84806eb61d300025178398f
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-10-19 23:15:03 -0300

    Updates

--- FIIs.py
+++ FIIs.py
@@ -2,13 +2,12 @@ import pandas as pd
 
 class FIIs:
   list = []
-  listFile = '../assets/fundosImobiliariosListadosNaB3.csv'
+  listFile = '../assets/fundosImobiliariosListadosNaB3.xlsx'
   def __init__(self):
     pass
 
   def setFiiCodes(self):
-    self.listFile = pd.read_csv(self.listFile)
-    print(self.listFile)
+    self.listFile = pd.read_excel(self.listFile)
 
 fii = FIIs()
 fii.setFiiCodes()
